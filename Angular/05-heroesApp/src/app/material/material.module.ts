db76c1580f2a4c2df285bc4d6585612c54d701af
Adrian Munoz <adrianmunozperez98@gmail.com>
2021-11-08 14:04:20 +0100

    material autocomplete

--- Angular/05-heroesApp/src/app/material/material.module.ts
+++ Angular/05-heroesApp/src/app/material/material.module.ts
@@ -5,6 +5,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
@@ -13,6 +16,9 @@ import {MatListModule} from '@angular/material/list';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
 
   ]
 })
