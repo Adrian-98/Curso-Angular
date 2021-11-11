6fe6753a46c8bf564c054e1129d35a0fe3e1162b
Adrian Munoz <adrianmunozperez98@gmail.com>
2021-11-11 12:10:38 +0100

    empezando modulo 18

--- Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('Rtx 4080ti')
+  });
 }
